0c5adce93565997c089233ea13d95e050fcee333
raphael <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-09-22 08:11:20 -0300

    Create modelos_tb.py

---
+++ Informações via python/modelos_tb.py
@@ -0,0 +1,10 @@
+print("=== MODELO ===")
+
+nome_modelo = input("Nome do modelo: ")
+ano_lancamento = input("Ano de lançamento: ")
+categoria = input("Categoria: ")
+
+print("\n=== DADOS MODELO ===")
+print(f"Nome do modelo: {nome_modelo}")
+print(f"Ano de lançamento: {ano_lancamento}")
+print(f"Categoria: {categoria}")
