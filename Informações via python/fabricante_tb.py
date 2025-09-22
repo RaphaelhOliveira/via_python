deb2b69fddbd72f07f340ca77e134157af13b5fb
raphael <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-09-22 08:08:39 -0300

    Create fabricante_tb.py

---
+++ Informações via python/fabricante_tb.py
@@ -0,0 +1,10 @@
+print("=== FABRICANTE ===")
+
+nome = input("Nome do fabricante: ")
+pais_origem = input("País de origem: ")
+telefone_contato = input("Telefone de contato: ")
+
+print("\n=== DADOS FABRICANTE ===")
+print(f"Nome: {nome}")
+print(f"País de origem: {pais_origem}")
+print(f"Telefone de contato: {telefone_contato}")
