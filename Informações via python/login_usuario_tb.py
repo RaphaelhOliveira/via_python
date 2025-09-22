724759644758f616e5cff971f8b439ee8fea0274
raphael <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-09-22 07:59:23 -0300

    Create login_usuario_tb.py

---
+++ Informações via python/login_usuario_tb.py
@@ -0,0 +1,8 @@
+print("=== LOGIN USUÁRIO ===")
+
+usuario = input("Usuário: ")
+senha = input("Senha: ")
+
+print("\n=== DADOS CADASTRADOS ===")
+print(f"Usuário: {usuario}")
+print(f"Senha: {senha}")
