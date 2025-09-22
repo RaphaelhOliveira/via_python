4709adb6d1a22720513c4ec20dcb47b224f72b38
raphael <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-09-22 08:15:16 -0300

    Create test_driv_tb.py

---
+++ Informações via python/test_driv_tb.py
@@ -0,0 +1,8 @@
+print("=== AGENDAMENTO DE TEST DRIVE ===")
+
+data_agendamento = input("Data de agendamento (DD/MM/AAAA HH:MM): ")
+resultado = input("Resultado: ")
+
+print("\n=== DADOS CADASTRADOS ===")
+print(f"Data de agendamento: {data_agendamento}")
+print(f"Resultado: {resultado}")
