95f295e598b51a27bdd1357f96f8c3ab1d659020
raphael <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-09-24 09:35:07 -0300

    Update test_driv_tb.py

--- Informações via python/test_driv_tb.py
+++ Informações via python/test_driv_tb.py
@@ -1,8 +1,21 @@
+test_drives = []
+ultimo_id_test_drive = 0
+
 print("=== AGENDAMENTO DE TEST DRIVE ===")
+
+ultimo_id_test_drive += 1
+id_test_drive = ultimo_id_test_drive
 
 data_agendamento = input("Data de agendamento (DD/MM/AAAA HH:MM): ")
 resultado = input("Resultado: ")
 
+test_drives.append({
+    'id': id_test_drive,
+    'data_agendamento': data_agendamento,
+    'resultado': resultado
+})
+
 print("\n=== DADOS CADASTRADOS ===")
+print(f"ID: {id_test_drive}")
 print(f"Data de agendamento: {data_agendamento}")
 print(f"Resultado: {resultado}")
