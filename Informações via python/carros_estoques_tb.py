019f1a44ce03e9543ddf5fddfc75c14aaad067e1
raphael <159036431+RaphaelhOliveira@users.noreply.github.com>
2025-09-24 08:58:22 -0300

    Update carros_estoques_tb.py

--- Informações via python/carros_estoques_tb.py
+++ Informações via python/carros_estoques_tb.py
@@ -1,4 +1,10 @@
+carros_estoque = []
+ultimo_id_carro = 0
+
 print("=== CARRO EM ESTOQUE ===")
+
+ultimo_id_carro += 1
+id_carro = ultimo_id_carro
 
 cor = input("Cor: ")
 chassi = input("Chassi: ")
@@ -7,7 +13,18 @@ preco_compra = input("Preço de compra: R$ ")
 preco_venda = input("Preço de venda: R$ ")
 status = input("Status: ")
 
+carros_estoque.append({
+    'id': id_carro,
+    'cor': cor,
+    'chassi': chassi,
+    'ano_fabricacao': ano_fabricacao,
+    'preco_compra': preco_compra,
+    'preco_venda': preco_venda,
+    'status': status
+})
+
 print("\n=== DADOS CADASTRADOS ===")
+print(f"ID: {id_carro}")
 print(f"Cor: {cor}")
 print(f"Chassi: {chassi}")
 print(f"Ano de fabricação: {ano_fabricacao}")
